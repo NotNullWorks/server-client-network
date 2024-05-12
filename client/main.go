3e2037f71eb5b7314a7592ff138f4fa0265bb499
Mohammad Khafid Barokum <navigator619@gmail.com>
2024-05-12 12:46:30 +0700

    change code to tcp call

--- client/main.go
+++ client/main.go
@@ -6,38 +6,30 @@ import (
 )
 
 func main() {
-	// Resolve UDP address and port
-	addr, err := net.ResolveUDPAddr("udp", "localhost:8080")
+	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
-		fmt.Println("Error resolving address:", err)
-		return
-	}
-
-	// Create UDP connection
-	conn, err := net.DialUDP("udp", nil, addr)
-	if err != nil {
-		fmt.Println("Error creating UDP connection:", err)
+		fmt.Println("Error connecting to server:", err)
 		return
 	}
 	defer conn.Close()
 
-	// Send message to server
-	message := []byte("Hello from client!")
-	_, err = conn.Write(message)
+	message := "Hello from client!"
+	_, err = conn.Write([]byte(message))
 	if err != nil {
-		fmt.Println("Error sending message to server:", err)
+		fmt.Println("Error sending data to server:", err)
 		return
 	}
 
-	fmt.Println("Message sent to server.")
+	fmt.Println("Data sent to server")
 
-	// Receive response from server
+	// Read response from server
 	buffer := make([]byte, 1024)
-	n, _, err := conn.ReadFromUDP(buffer)
+	n, err := conn.Read(buffer)
 	if err != nil {
 		fmt.Println("Error reading response from server:", err)
 		return
 	}
 
-	fmt.Println("Response from server:", string(buffer[:n]))
+	response := string(buffer[:n])
+	fmt.Println("Response from server:", response)
 }
--- server/main.go
+++ server/main.go
@@ -5,43 +5,52 @@ import (
 	"net"
 )
 
-func main() {
-	// Resolve UDP address and port
-	addr, err := net.ResolveUDPAddr("udp", ":8080")
-	if err != nil {
-		fmt.Println("Error resolving address:", err)
-		return
-	}
-
-	// Create UDP connection
-	conn, err := net.ListenUDP("udp", addr)
-	if err != nil {
-		fmt.Println("Error creating UDP connection:", err)
-		return
-	}
+func handleClient(conn net.Conn) {
 	defer conn.Close()
 
-	fmt.Println("Server started. Waiting for messages...")
-
-	// Buffer for incoming messages
 	buffer := make([]byte, 1024)
-
 	for {
-		// Read from UDP connection
-		n, addr, err := conn.ReadFromUDP(buffer)
+		n, err := conn.Read(buffer)
 		if err != nil {
-			fmt.Println("Error reading from UDP connection:", err)
+			fmt.Println("Error reading from client:", err)
 			return
 		}
 
-		fmt.Printf("Received message from %s: %s\n", addr.String(), string(buffer[:n]))
+		message := string(buffer[:n])
+		fmt.Println("Received from client:", message)
 
-		// Respond to the client
-		response := []byte("Hello from server!")
-		_, err = conn.WriteToUDP(response, addr)
+		// Process the received message (e.g., parse JSON, perform operations)
+
+		// Send a response back to the client
+		response := []byte("Response from server: " + message)
+		_, err = conn.Write(response)
 		if err != nil {
 			fmt.Println("Error sending response to client:", err)
 			return
 		}
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server started. Waiting for connections...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			return
+		}
+
+		fmt.Println("Client connected:", conn.RemoteAddr())
+
+		// Handle client connection in a goroutine
+		go handleClient(conn)
 	}
 }
